c50c2bc1d47ee80bb07daede8aad84ef9fa1ee6b
Sanal Parakkal <sanalpk220@gmail.com>
2021-12-06 20:49:58 +0530

    SplashScren added

---
+++ lib/splashScreen.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors, file_names
+import 'dart:async';
+
+import 'package:alpha/screens/LoginScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 1000000), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          LottieBuilder.asset('assets/Splash.json', repeat: true),
+        ],
+      ),
+    );
+  }
+}
